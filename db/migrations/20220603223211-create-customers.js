baede3a4cdd0382a060077ff88ae8380628bd79c
Fabian Gutierrez <fabiangtml@gmail.com>
2022-06-25 14:57:41 -0500

    fix migrations typo error

--- db/migrations/20220603223211-create-customers.js
+++ db/migrations/20220603223211-create-customers.js
@@ -12,37 +12,37 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: DataTypes.INTEGER,
-    },
-    name: {
-        allowNull: false,
-        type: DataTypes.STRING,
-    },
-    lastName: {
-        allowNull: false,
-        type: DataTypes.STRING,
-        field: 'last_name',
-    },
-    phone: {
-        allowNull: true,
-        type: DataTypes.STRING,
-    },
-    createdAt: {
-        allowNull: false,
-        type: DataTypes.DATE,
-        field: 'create_at',
-        defaultValue: Sequelize.NOW
-    },
-    userId: {
-        field: 'user_id',
-        allowNull: false,
-        type: DataTypes.INTEGER,
-        references: {
-            model: USER_TABLE,
-            key: 'id'
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-    },
+      },
+      name: {
+          allowNull: false,
+          type: DataTypes.STRING,
+      },
+      lastName: {
+          allowNull: false,
+          type: DataTypes.STRING,
+          field: 'last_name',
+      },
+      phone: {
+          allowNull: true,
+          type: DataTypes.STRING,
+      },
+      createdAt: {
+          allowNull: false,
+          type: DataTypes.DATE,
+          field: 'create_at',
+          defaultValue: Sequelize.NOW
+      },
+      userId: {
+          field: 'user_id',
+          allowNull: false,
+          type: DataTypes.INTEGER,
+          references: {
+              model: USER_TABLE,
+              key: 'id'
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'SET NULL',
+      },
     });
   },
 
